294c559e8520b7d3903f821580fccb90dfb99092
Nidhi <nidhipatel439@gmail.com>
2022-04-28 18:20:51 -0400

    use array for book data

--- components/card.tsx
+++ components/card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-const Card = (props: { imageAlt: string, bookTitle: string, bookDescription: string, image: object, bookLink: string }) => {
+const Card = (props: { imageAlt: string, bookTitle: string, bookDescription: string, image: string, bookLink: string }) => {
   return (
     <div className="card mb-3 w-75 mx-auto">
       <div className="row g-0">
@@ -15,7 +15,10 @@ const Card = (props: { imageAlt: string, bookTitle: string, bookDescription: str
           <div className="card-body">
             <h5 className="card-title">{props.bookTitle}</h5>
             <p className="card-text">{props.bookDescription}</p>
-            <a href={props.bookLink} className="card-link" target="_blank" rel="noopener noreferrer">Amazon Link</a>
+            <a href={props.bookLink} target="_blank" rel="noopener noreferrer">
+              <button className="btn btn-warning card-link" >Amazon Link
+              </button>
+            </a>
           </div>
         </div>
       </div>

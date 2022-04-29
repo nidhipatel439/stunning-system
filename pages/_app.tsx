0f016eeb73eb2aa208371da8848fefcb8a6db17a
Nidhi <nidhipatel439@gmail.com>
2022-04-29 15:47:36 -0400

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,8 +10,21 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
     </Head>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></Script>
+
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+    <Script strategy="lazyOnload" id="">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+    </Script>
     <Component {...pageProps} />
   </>
 }
---
+++ pages/about/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>About
+
+                </title>
+            </Head>
+            <div>About</div>
+        </>
+    )
+}
+
+export default About
